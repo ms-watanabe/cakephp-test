06c3edaaa27eb6df11bda77f13f0198f44c0ad24
ms-watanabe <y.watanabe@miraistart.com>
2017-01-30 18:58:29 +0900

    テストページ追加

--- config/routes.php
+++ config/routes.php
@@ -50,7 +50,7 @@ Router::scope('/', function (RouteBuilder $routes) {
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
     $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
-
+	$routes->connect('/test',['controller' =>'Test','action' =>'index']);
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
      */
---
+++ src/Template/Layout/test.ctp
@@ -0,0 +1,20 @@
+<?php
+//test.ctp
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?= $this->Html->charset() ?>
+ 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+    </title>
+    <?= $this->Html->meta('icon') ?>
+ 
+ 
+</head>
+<body>
+  <?= $this->fetch('content') ?>
+ 
+</body>
+</html>
